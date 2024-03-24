2ee129d2b170d30d1adf8c089492653684061281
Arsen Hryhoriak <arsen.hryh@gmail.com>
2024-03-25 01:16:42 +0200

    faq tablet responsive

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -7,7 +7,7 @@ const Faq = ({ isTablet }) => {
   return (
     <section
       id="faq"
-      className="px-8 flex flex-col items-center mb-[60px] md:mb-[80px]"
+      className="px-8 md:w-[592px] md:mx-auto flex flex-col items-center md:items-center mb-[60px] md:mb-[80px]"
     >
       <h2 className="text-[44px] md:text-[80px] text-white font-black leading-[0.91] mb-[18px]">
         FAQ
@@ -15,7 +15,7 @@ const Faq = ({ isTablet }) => {
       {faqs.map((faq, index) => (
         <div
           key={index}
-          className={`p-2 rounded-xl w-[75%] md:w-[592px] flex gap-2 group ${
+          className={`p-2 md:pt-[18px] md:pb-[24px] rounded-xl w-[75%] md:w-[592px] flex  gap-2 md:gap-6 group md:relative ${
             openIndex === index && "bg-dark"
           }`}
           onClick={() => setOpenIndex(index)}
@@ -24,13 +24,13 @@ const Faq = ({ isTablet }) => {
             <img
               src={`./questions/question${index + 1}.webp`}
               alt="ape"
-              className={`w-[148px] h-[183px] transform rotate-[-12deg] transition-all duration-500 ease-in-out ${
+              className={`w-[148px] h-[183px] transform rotate-[-12deg] transition-all duration-500 ease-in-out rounded-2xl absolute top-0 left-[15px] ${
                 openIndex === index ? "max-h-64" : "max-h-0"
               }`}
             />
           )}
           <span
-            className={`font-biro  text-sm whitespace-nowrap  ${
+            className={`font-biro text-sm md:text-base whitespace-nowrap md:ml-[170px]  ${
               openIndex === index
                 ? "text-white"
                 : "text-accent group-hover:text-white"
@@ -40,7 +40,7 @@ const Faq = ({ isTablet }) => {
           </span>
           <div>
             <div
-              className={`font-bold text-xl md:w-[355px] cursor-pointer duration-150 leading-none ${
+              className={`font-bold text-xl md:text-[32px] cursor-pointer duration-150 leading-none ${
                 openIndex === index
                   ? "text-accent"
                   : "text-white group-hover:text-accent"
@@ -49,7 +49,7 @@ const Faq = ({ isTablet }) => {
               {faq.question}
             </div>
             <div
-              className={`mt-[10px] font-messina font-thin text-white text-xs uppercase transition-all duration-500 ease-in-out overflow-hidden leading-[1.17] ${
+              className={`mt-[10px] font-messina font-light text-white text-xs uppercase transition-all duration-500 ease-in-out overflow-hidden leading-[1.17] ${
                 openIndex === index ? "max-h-64" : "max-h-0"
               }`}
             >
